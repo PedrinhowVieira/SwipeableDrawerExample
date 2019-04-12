8f99572960ea4305063a973fb4ca34c83ce4e570
PedrinhowVieira <pedro.dmvieira@gmail.com>
2019-04-12 14:21:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import ListItem from "@material-ui/core/ListItem";
+import Button from "@material-ui/core/Button";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import ListItemText from '@material-ui/core/ListItemText';
+
 
 class App extends Component {
+  state = {
+    left: false,
+    dashboard: 'none_yet',
+  };
+  
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
+  changeDashboardState = (dashboardValue) => {
+    this.setState({
+      dashboard: dashboardValue,
+    }) 
+  }
+  
   render() {
+    const sideList = (
+      <Fragment>
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}onClick={() => this.changeDashboardState(text)}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Fragment>
+    );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div>
+        <Button onClick={this.toggleDrawer("left", true)}>Open Left</Button>
+        <p>{this.state.dashboard}</p>
+        <SwipeableDrawer
+          open={this.state.left}
+          onClose={this.toggleDrawer("left", false)}
+          onOpen={this.toggleDrawer("left", true)}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer("left", false)}
+            onKeyDown={this.toggleDrawer("left", false)}
           >
-            Learn React
-          </a>
-        </header>
+            {sideList}
+          </div>
+        </SwipeableDrawer>
       </div>
     );
   }
